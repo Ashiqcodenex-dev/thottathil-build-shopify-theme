73abe54be1ec2e1dc40617ca0c1a75e88d89ac4c
Ashiq Kakkt <ashiq.codenex@gmail.com>
2026-03-02 09:46:03 +0530

    feat: Initialize new Shopify theme structure with Tailwind CSS integration.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
